63fbbb2679d0822f39267c2e46657ed6dcc821f9
adarshrajs <adarshrajs872@gmail.com>
2023-02-18 15:59:53 +0530

    added

---
+++ movieproject/movieapp/views.py
@@ -0,0 +1,48 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import MovieForm
+
+from . models import Movie
+# Create your views here.
+
+
+def index(requset):
+    movie = Movie.objects.all()
+    context = {
+        'movie_list': movie
+    }
+
+    return render(requset, 'index.html', context)
+
+
+def details(request, movie_id):
+    m = Movie.objects.get(id=movie_id)
+    return render(request, "details.html", {'movie': m})
+
+
+def add_movie(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        desc = request.POST.get('desc')
+        year = request.POST.get('year')
+        image = request.FILES['image']
+        movie = Movie(name=name, desc=desc, year=year, image=image)
+        movie.save()
+    return render(request, 'add.html')
+
+
+def update(request, id):
+    movie = Movie.objects.get(id=id)
+    form = MovieForm(request.POST or None, request.FILES, instance=movie)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    return render(request, 'edit.html', {'form':form, 'movie':movie})
+
+
+def delete(request, id):
+    if request.method == 'POST':
+        movie = Movie.object.get(id=id)
+        movie.delet()
+        return redirect('/')
+    return render(request, 'delete.html')
